13e989d7a4069403a00ad728eb49d910d173df00
Matias Valdenegro <matias.valdenegro@gmail.com>
2020-02-02 20:42:04 +0100

    Add support for DropConnect layers for VGG16

--- dnnbuild/models/vgg16.py
+++ dnnbuild/models/vgg16.py
@@ -2,6 +2,23 @@
 
 from keras import layers, models, backend
 
+# Wrappers to switch to DropConnect layers
+def conv2d_wrapper(filters, kernel_size, prob=0.0, regularization='bn', **kwargs):
+    if regularization is 'dropconnect':
+        from keras_uncertainty.layers import DropConnectConv2D
+
+        return DropConnectConv2D(filters, kernel_size, prob=prob, **kwargs)
+
+    return layers.Conv2D(filters, kernel_size, **kwargs)
+
+def dense_wrapper(units, prob=0.0, regularization='bn', **kwargs):
+    if regularization is 'dropconnect':
+        from keras_uncertainty.layers import DropConnectDense
+
+        return DropConnectDense(units, prob=prob, **kwargs)
+    else:
+        return layers.Dense(units, **kwargs)
+
 def VGG16(input_shape,
           include_top=True,
           regularization='bn',
@@ -11,7 +28,7 @@ def VGG16(input_shape,
           neurons_fc2=4096,
           pooling=None,
           classes=10):
-    assert regularization in [None, 'dropout', 'bn']
+    assert regularization in [None, 'dropout', 'bn', 'dropconnect']
     assert pooling in [None, 'avg', 'max']
 
     inp = layers.Input(shape=input_shape)
@@ -19,18 +36,22 @@ def VGG16(input_shape,
     bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
 
     # Block 1
-    x = layers.Conv2D(64, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block1_conv1')(inp)
+    x = conv2d_wrapper(64, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block1_conv1',
+                       prob=dropout_prob,
+                       regularization=regularization)(inp)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(64, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block1_conv2')(x)
+    x = conv2d_wrapper(64, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block1_conv2',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
@@ -38,18 +59,22 @@ def VGG16(input_shape,
     x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
-    x = layers.Conv2D(128, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block2_conv1')(x)
+    x = conv2d_wrapper(128, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block2_conv1',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(128, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block2_conv2')(x)
+    x = conv2d_wrapper(128, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block2_conv2',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
@@ -57,25 +82,32 @@ def VGG16(input_shape,
     x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
     # Block 3
-    x = layers.Conv2D(256, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block3_conv1')(x)
+    x = conv2d_wrapper(256, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block3_conv1',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(256, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block3_conv2')(x)
+    x = conv2d_wrapper(256, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block3_conv2',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
+
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(256, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block3_conv3')(x)
+    x = conv2d_wrapper(256, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block3_conv3',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
@@ -83,26 +115,32 @@ def VGG16(input_shape,
     x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Block 4
-    x = layers.Conv2D(512, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block4_conv1')(x)
+    x = conv2d_wrapper(512, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block4_conv1',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(512, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block4_conv2')(x)
+    x = conv2d_wrapper(512, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block4_conv2',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(512, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block4_conv3')(x)
+    x = conv2d_wrapper(512, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block4_conv3',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
@@ -110,46 +148,53 @@ def VGG16(input_shape,
     x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
     # Block 5
-    x = layers.Conv2D(512, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block5_conv1')(x)
+    x = conv2d_wrapper(512, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block5_conv1',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(512, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block5_conv2')(x)
+    x = conv2d_wrapper(512, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block5_conv2',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
 
     if regularization is 'bn':
         x = layers.BatchNormalization(axis=bn_axis)(x)
 
-    x = layers.Conv2D(512, (3, 3),
-                      activation='relu',
-                      padding='same',
-                      name='block5_conv3')(x)
+    x = conv2d_wrapper(512, (3, 3),
+                       activation='relu',
+                       padding='same',
+                       name='block5_conv3',
+                       prob=dropout_prob,
+                       regularization=regularization)(x)
+
     x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     if include_top:
         # Classification block
         x = layers.Flatten(name='flatten')(x)
-        x = layers.Dense(neurons_fc1, activation='relu', name='fc1')(x)
+        x = dense_wrapper(neurons_fc1, activation='relu', name='fc1', prob=dropout_prob, regularization=regularization)(x)
 
         if regularization is 'bn':
             x = layers.BatchNormalization()(x)
         elif regularization is 'dropout':
             x = layers.Dropout(rate=dropout_prob, noise_shape=dropout_noise_shape)(x)
 
-        x = layers.Dense(neurons_fc2, activation='relu', name='fc2')(x)
+        x = dense_wrapper(neurons_fc2, activation='relu', name='fc2', prob=dropout_prob, regularization=regularization)(x)
 
         if regularization is 'bn':
             x = layers.BatchNormalization()(x)
         elif regularization is 'dropout':
             x = layers.Dropout(rate=dropout_prob, noise_shape=dropout_noise_shape)(x)
 
-        x = layers.Dense(classes, activation='softmax', name='predictions')(x)
+        x = dense_wrapper(classes, activation='softmax', name='predictions', prob=dropout_prob, regularization=regularization)(x)
     else:
         if pooling == 'avg':
             x = layers.GlobalAveragePooling2D()(x)
